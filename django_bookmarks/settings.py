67822887fe9a88960002bf070ab9d4e53dcdc5eb
Robert Flaugher <robertf57@gmail.com>
2012-05-26 19:08:50 -0700

    Generated first user bookmark page (end of ch. 2)

--- django_bookmarks/settings.py
+++ django_bookmarks/settings.py
@@ -1,5 +1,4 @@
 # Django settings for django_bookmarks project.
-import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -111,7 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), 'templates'),
+    '/Users/flaugher/code/django/django_bookmarks/templates'
 )
 
 INSTALLED_APPS = (
--- django_bookmarks/urls.py
+++ django_bookmarks/urls.py
@@ -16,4 +16,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     (r'^$', main_page),
+    (r'^user/(\w+)/$', user_page),
 )
