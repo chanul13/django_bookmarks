c949951d178cec46541dcf1eb3976e7b5b495a09
Robert Flaugher <robertf57@gmail.com>
2012-05-31 16:14:41 -0700

    Added logout page

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,13 +1,22 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import Context
 from django.template.loader import get_template
 from django.contrib.auth.models import User
+from django.shortcuts import render_to_response
+from django.contrib.auth import logout
 
+'''
 def main_page(request):
     template = get_template('main_page.html')
     variables = Context({ 'user': request.user })
     output = template.render(variables)
     return HttpResponse(output)
+'''
+def main_page(request):
+    return render_to_response(
+        'main_page.html',
+        { 'user': request.user }
+    )
 
 def user_page(request, username):
     try:
@@ -24,3 +33,7 @@ def user_page(request, username):
     })
     output = template.render(variables)
     return HttpResponse(output)
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
