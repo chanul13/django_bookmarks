9657f57cf2e40dd53f23ecd84f2b7789d83b1898
Robert Flaugher <robertf57@gmail.com>
2012-06-22 20:13:11 -0700

    Added code to support unit testing

--- bookmarks/tests.py
+++ bookmarks/tests.py
@@ -6,7 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from django.test.client import Client
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +14,40 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+class ViewTest(TestCase):
+    fixtures = ['test_data.json']
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_register_page(self):
+        data = {
+            'username': 'test_user',
+            'email': 'test_user@example.com',
+            'password1': 'pass123',
+            'password2': 'pass123'
+        }
+
+        response = self.client.post('/register/', data)
+        self.assertRedirects(response, '/register/success/')
+
+    def test_bookmark_save(self):
+        response = self.client.login(
+            username = 'flaugher',
+            password = 'flaugher'
+        )
+        self.assertTrue(response, msg = 'Failed to login.')
+
+        data = {
+            'url'  : 'http://www.example.com/',
+            'title': 'Test URL',
+            'tags' : 'test-tag'
+        }
+        response = self.client.post('/save/', data)
+        self.assertRedirects(response, '/user/flaugher/')
+
+        response = self.client.get('/user/flaugher/')
+        self.assertContains(response, 'http://www.example.com/')
+        self.assertContains(response, 'Test URL')
+        self.assertContains(response, 'test-tag')
